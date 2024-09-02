3985ea9e579e7aa9834aca11910c8c4f3a61e7cb
Danilov Alex <danilov.alexandr@hotmail.com>
2024-09-02 21:56:06 +0500

    Домашнее задание по теме "Структура проекта. Маршруты и модели Pydantic."

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers import task,user
+
+app = FastAPI()
+app.include_router(task.router)
+app.include_router(user.router)
+
+@app.get('/')
+async def welcom():
+    return {"message": "Welcome to Taskmanager"}
