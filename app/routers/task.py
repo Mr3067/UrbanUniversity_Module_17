98b653dc2d82177a5fce36dc96a0182610616d18
Danilov Alex <danilov.alexandr@hotmail.com>
2024-09-03 22:07:26 +0500

    Домашнее задание по теме "Модели SQLALchemy. Отношения между таблицами."

--- app/routers/task.py
+++ app/routers/task.py
@@ -1,4 +1,9 @@
 from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey,Integer, String,Boolean,Float
+from sqlalchemy.orm import relationship
+
+from user import User
 
 router = APIRouter(prefix='/task', tags=['task'])
 
@@ -21,3 +26,20 @@ async def update_task():
 @router.get('/delete')
 async def delete_task():
     pass
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content  = Column(String)
+    priority = Column(Integer,default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
+    slug = Column(String,unique=True,index=True)
+
+    user  = relationship("User", back_populates='tasks')
+
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,23 +1,51 @@
 from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship
+
+from task import Task
 
 router = APIRouter(prefix='/user', tags=['user'])
+
 
 @router.get('/')
 async def all_users():
     pass
 
+
 @router.get('/user_id')
 async def user_by_id():
     pass
+
 
 @router.get('/create')
 async def create_user():
     pass
 
+
 @router.get('/update')
 async def update_user():
     pass
 
+
 @router.get('/delete')
 async def delete_user():
     pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+
+    tasks = relationship("Task", back_populates='user')
+
+
+from sqlalchemy.schema import CreateTable
+
+print(CreateTable(User.__table__))
